746d8ed1c71e5d96d9fc1751529a3c1f0d33cbca
Henry School Email <henryborska@wustl.edu>
2021-12-23 16:17:12 -0500

    Updated contract to add prize money (50% chance)

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,46 +6,66 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    uint256 private seed;
+    bool winsPrize;
 
-    //Our wave event -- emitted when wave is called (informs the calling app about state of contract)
-    event NewWave(address indexed from, uint256 timestamp, string message);
+    event NewWave(address indexed from, uint256 timestamp, string message, bool isWinner);
 
     struct Wave {
-        address waver; // The address of the user who waved
-        string message; // The message the user sent
-        uint256 timestamp; // The timestamp when the user waved
+        address waver;
+        string message;
+        uint256 timestamp;
+        bool isWinner;
     }
 
-    //Declaring an array of waves
     Wave[] waves;
 
-    constructor() {
-        console.log("I AM SMART CONTRACT. POG.");
+    constructor() payable {
+        console.log("We have been constructed!");
+        
+        //Initial seed (for randomness)
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
-    /*
-     * You'll notice I changed the wave function a little here as well and
-     * now it requires a string called _message. This is the message our user
-     * sends us from the frontend!
-     */
+    //Generating random # based on the block difficulty and timestamp 
+    //NOT a good way to generate randomness since hackers could reverse engineer! 
+    //Seed makes it harder but still possible to hack, but this is fine for our tiny app
     function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s waved w/ message %s", msg.sender, _message);
+        console.log("%s has waved!", msg.sender);
 
-        //Storing the wave data in an array
-        waves.push(Wave(msg.sender, _message, block.timestamp));
+        //New seed # for next user that waves
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        
+        console.log("Random # generated: %d", seed);
 
-        //Emitting our new wave event
-        emit NewWave(msg.sender, block.timestamp, _message);
+        //50/50 chance the user wins the prize
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+            winsPrize = true;
+            waves.push(Wave(msg.sender, _message, block.timestamp, true));
+
+            //Sending the prize ether
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        } else {
+            winsPrize = false;
+            waves.push(Wave(msg.sender, _message, block.timestamp, false));
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message, winsPrize);
     }
 
-    //Returns our waves array to us (makes it easy on front end to get all waves)
     function getAllWaves() public view returns (Wave[] memory) {
         return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
-        console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
 }
